a7d824680d66d9586b8517ffb1644b275e77ef61
Wikan-dev <oneshootindo@gmail.com>
2025-08-04 17:40:38 +0800

    fix few bug

--- vite.config.cjs
+++ vite.config.cjs
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), [require('@tailwindcss/line-clamp')]],
+  plugins: [react(), tailwindcss(), [await import('@tailwindcss/line-clamp')]],
   base: "/webBasedHabitTracker",
 })
