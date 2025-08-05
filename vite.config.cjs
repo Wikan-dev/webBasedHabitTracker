5e580d5db64cf32fd18c46a7359b1134a7f41017
Wikan-dev <oneshootindo@gmail.com>
2025-08-05 09:51:01 +0800

    update

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,9 +1,8 @@
 const { defineConfig } = require('vite')
 const react = require('@vitejs/plugin-react')
-const tailwindcss = require('tailwindcss')
 const lineClamp = require('@tailwindcss/line-clamp')
 
 module.exports = defineConfig({
-  plugins: [react(), tailwindcss(), lineClamp],
+  plugins: [react(), lineClamp],
   base: "/webBasedHabitTracker", // untuk GitHub Pages
 })
